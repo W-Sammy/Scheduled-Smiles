6cc078095711c4eda71b662edd60f602a5c84d21
KeavnKeavn <keavnlor@csus.edu>
2024-11-04 22:05:05 -0800

    WIP Sign-in + dependency

--- js/InputBox.tsx
+++ js/InputBox.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "../css/InputBox.css"
 
+
 const InputBox = () => {
-  return (
-    <div className="TextBox">
-    <label htmlFor="email" className="label">
-      Email <span className="required">*</span>
-    </label>
-    <input type="email" id="email" className="input" placeholder="sophie@example.com"/>
-  </div>
-  )
+    
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (event: any) => {
+        setInputValue(event.target.value);
+    };
+
+    return (
+        <div className="TextBox">
+        <label htmlFor="email" className="label">
+            Email <span className="required">*</span>
+        </label>
+        <input type="email" id="email" value={inputValue} onChange={handleChange} className="input" placeholder="sophie@example.com"/>
+
+        Input : {inputValue}
+
+        </div>
+    )
 }
 
 export default InputBox
--- js/SignInComponent.tsx
+++ js/SignInComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "../css/SignInComponent.css";
 import Button from "../js/Button.tsx"
 import TextBox from "../js/InputBox.tsx"
@@ -6,17 +6,21 @@ import InputBox from '../js/InputBox.tsx';
 
 
 const SignInComponent = () => {
-  return (<>
+
+
+return (<>
+    const [inputValue, setInputValue] = useState('');
     <div>Scheduled Smiles Sign in box</div>  
+    inputValue = InputBox();
     <div className="SignIn">
-      <div className="button1">
-        <Button children="Sign-in2"/>
-        <InputBox></InputBox>
-      </div>
+    <div className="button1">
+      <Button children="Sign-in2"/>
+      <InputBox/>
+    </div>
     </div>
     <Button>Sign-in</Button>
     </>
-  )
+)
 }
 
 export default SignInComponent
