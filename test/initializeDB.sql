85deeb5b0367065167b7997b2f6d6614334103fe
erdsmabilog <149103955+erdsmabilog@users.noreply.github.com>
2024-11-06 10:39:51 -0800

    Start initializeDB.sql

---
+++ test/initializeDB.sql
@@ -0,0 +1,6 @@
+INSERT INTO roleTypes
+VALUES (UNHEX(SHA2('Patient', 256)), 'Patient');
+INSERT INTO roleTypes
+VALUES (UNHEX(SHA2('Staff', 256)), 'Staff');
+INSERT INTO roleTypes
+VALUES (UNHEX(SHA2('Admin', 256)), 'Admin');
