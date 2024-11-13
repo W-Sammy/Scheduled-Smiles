417514c32e59cc6b9e3c2b4d3679ee876812d9b0
W_Sammy <wongsammy23@gmail.com>
2024-11-13 09:01:02 -0800

    Updated Appointment class Attributes & Constructor

--- backend/Appointment.java
+++ backend/Appointment.java
@@ -1,18 +1,30 @@
+import java.util.ArrayList;
 public class Appointment{
     //private attributes
-    private byte[] typeID = new byte[32];
-    private int timestamp;
-    private boolean completionStatus;
-    private boolean cancelStatus;
-    private double cost;
+    private byte[] typeID = new byte[32];	//ID assigned to each appointment
+    private byte[] patientID = new byte[32];	//Who the patient this appointment is for
+    private ArrayList<byte[]> staffList = new ArrayList<>();	//list of staff who worked on this appointment, dynamic storage should not cause errors unless information is somehow manipulated in backend
+    private int stationNumber;	//Which station the appointment took place in; reflects an input accepted from frontend
+    private String treatment;	//Which treatment was given to the patient, finalizing as singular treatment option only, also used to calculate cost of treatment for billing
+    private ArrayList<String> healthCondition = new ArrayList<>();	//list of possible health conditions retreived from frontend; can have mulitple or none
+    private String notes;	//Singular input box for notes about the patient written by staff; retrieved from frontend 
+    private int timestamp;	//Scheduled start time of the appointment
+    private boolean completionStatus;	//Check if appointment has been completed
+    private boolean cancelStatus;	//Check if appointment has been cancelled
 
     //constructor
-    public Appointment(byte[] typeID, int timestamp, boolean completionStatus, boolean cancelStatus, double cost) {
+    public Appointment(byte[] typeID, byte[] patientID, ArrayList<byte[]> staffList, int stationNumber, String treatment,
+						ArrayList<String> healthCondition, String notes, int timestamp, boolean completionStatus, boolean cancelStatus) {
         this.typeID = typeID;
-        this.timestamp = timestamp;
-        this.completionStatus = completionStatus;
-        this.cancelStatus = cancelStatus;
-        this.cost = cost;
+		this.patientID = patientID;
+		this.staffList = staffList;
+		this.stationNumber = stationNumber;
+		this.treatment = treatment;
+		this.healthCondition = healthCondition;
+		this.notes = notes;
+		this.timestamp = timestamp;
+		this.completionStatus = completionStatus;
+		this.cancelStatus = cancelStatus;
     }
 
 
