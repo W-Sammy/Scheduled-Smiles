89a6c330b3652124376b70bf5298511ec828e81b
Kyle <kaisonpham@gmail.com>
2024-11-22 21:43:59 -0800

    Modified batch script to work from root

--- backend/Server/runServer.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d %~dp0..\..
-java -cp ./backend/Server/;./backend/Server/lib/gson-2.11.0.jar;./backend/Server/lib/mysql-connector-j-9.1.0.jar ./backend/Server/Server.java "./frontend/"
-pause
