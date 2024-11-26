d1552ae7fb42f72e8c6e1fc8fe035bc9469ffde0
dmanganti <dannmanganti@gmail.com>
2024-11-26 13:43:19 -0800

    Added history mapping

--- backend/Server/Enum/Pages.java
+++ backend/Server/Enum/Pages.java
@@ -1,4 +1,5 @@
 package Server.Enum;
+
 // This is a map of all accepted pages to redirect to upon url request. Note that these map from url path to fileNAME only, no paths! (might change at a later date) -Kyle
 import java.util.Map;
 import java.util.*;
@@ -7,15 +8,16 @@ public class Pages {
     private Pages() {
         // restrict instantiation -Kyle
     }
+
     // map of arg limit is 10, so... no more pages
     public static final Map<String, String> MAPPED_FILES = Map.of(
-        "/", "index.html",
-        "404", "404.html",
-        "payroll", "payroll.html",
-        "session", "session.html",
-        "dashboard", "dashboard.html",
-        "calendar", "calendar.html",
-        "billing", "billing.html",
-        "chat", "chat.html"
-    );
+            "/", "index.html",
+            "404", "404.html",
+            "payroll", "payroll.html",
+            "session", "session.html",
+            "dashboard", "dashboard.html",
+            "calendar", "calendar.html",
+            "billing", "billing.html",
+            "chat", "chat.html",
+            "history", "history.html");
 }
