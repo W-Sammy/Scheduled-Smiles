dabd4290bb9cad4bf0d1a789d170bc025789437b
Kyle <kaisonpham@gmail.com>
2024-11-26 07:00:23 -0800

    this is fucked

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -181,6 +181,7 @@ async function getFullName(userID) {
     const endpoint = "/api/database/get"
     const method = "POST"
     let response = await request(requestBody, endpoint, method)
+    response = (response == "[]") ? null : response
     return response
 }
 
