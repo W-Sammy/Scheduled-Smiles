10ebc5dd40503b426d9effeb214c393217337465
Kyle <kaisonpham@gmail.com>
2024-11-28 01:00:35 -0800

    Implemented loading messages on frontend

--- frontend/js/chat.js
+++ frontend/js/chat.js
@@ -8,19 +8,38 @@ function loadData() {
         showWarning("Not logged in!", 3, "bottom")
         return
     }
-    getChats(getCookieValue("userID")).then(response => {
-        // this code wont work
-        // TODO: find out how to match this fuckass format of pairID,
-        //  one chat has personA->personB, another chat has personB->personA
-        //  need to figure how to pair up the array values for corrosponding chats
+    const userID = getCookieValue("userID")
+    getChats(userID).then(response => {
+        const chats = {}
+        // Merge chats
         JSON.parse(response).forEach(values => {
             const {messages, pairID, receiverID, senderID} = values
-            console.log(pairID, receiverID, senderID)
-            const contactID = getOtherUser(receiverID, senderID)
-            loadContact(contactID)
-            messages.sort((a, b) => a.createdAt - b.createdAt).forEach(message => {
-                message.content
-            })
+            const sortedMessages = messages.sort((a, b) => a.createdAt - b.createdAt)
+            const otherContact = getOtherUser(senderID, receiverID)
+            if (!chats.hasOwnProperty(otherContact)) {
+                chats[otherContact] = {sent: [], recieved: []}
+                loadContact(otherContact)
+            }
+            if (senderID == userID)
+                chats[otherContact].sent = sortedMessages
+            else
+                chats[otherContact].recieved = sortedMessages
+        })
+
+        // Display values
+        Object.keys(chats, id => {
+            const sent = chats[id].sent
+            const recieved = chats[id].recieved
+            while (sent.length > 0 || recieved.length > 0) {
+                if (!sent.length)
+                    appendMessage(recieved.pop().textContent, "left")
+                else if (!recieved.length)
+                    appendMessage(sent.pop().textContent, "right")
+                else if (sent[sent.length - 1].createdAt > recieved[recieved.length - 1].createdAt)
+                    appendMessage(sent.pop().textContent, "right")
+                else
+                    appendMessage(recieved.pop().textContent, "left")
+            }
         })
     })
 }
@@ -60,4 +79,28 @@ function createContact(fullName) {
 function clearData() {
     document.getElementById("contacts-list").innerHTML = ""
     document.getElementById("message-container").innerHTML = ""
+    document.querySelector("#current-contact .contact-name").innerHTML = ""
+}
+
+function appendMessage(messageContent, type) {
+    const chatWindow = document.querySelector('.chat-window')
+    const messageInput = document.querySelector('.message-input')
+
+    // Creates a new message as a div
+    const newMessageDiv = document.createElement('div')
+    newMessageDiv.classList.add('message') // adds classname 'message' for styling
+    newMessageDiv.classList.add(type)
+    newMessageDiv.textContent = messageContent;
+
+    // Appends the new message div in .chat-window
+    chatWindow.appendChild(newMessageDiv)
+}
+
+function appendNewMessage() {
+    // gets the input value from .message-input
+    const message = messageInput.value
+
+    appendMessage(message, "right")
+
+    messageInput.value = ''
 }
