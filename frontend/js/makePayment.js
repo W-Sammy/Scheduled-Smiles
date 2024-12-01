73b8728dc7c6a736903fd1562d7e831f32b19a0a
dmanganti <email@example.com>
2024-11-30 22:38:06 -0800

    made make payment overlay (ty keav'n)

---
+++ frontend/js/makePayment.js
@@ -0,0 +1,7 @@
+function openMakePayment() {
+    document.getElementById('make-payment-box').style.display = 'flex';
+}
+
+function closeMakePayment() {
+    document.getElementById('make-payment-box').style.display = 'none';
+}
