6483ef053c9e99901280cb51fea525fff89e01a8
Erds Ferdi Mabilog <erdsmabilog@gmail.com>
2024-12-02 17:42:47 -0800

    Slight cleanup -- removed makePayment.js (had functions move to billing.js)

--- frontend/js/makePayment.js
+++
@@ -1,7 +0,0 @@
-function openMakePayment() {
-    document.getElementById('make-payment-box').style.display = 'flex';
-}
-
-function closeMakePayment() {
-    document.getElementById('make-payment-box').style.display = 'none';
-}
